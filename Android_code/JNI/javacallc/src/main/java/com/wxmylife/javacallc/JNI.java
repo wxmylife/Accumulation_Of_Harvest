baeebaeffdb9968f25b2431baae75d2d1b563f09
wxmylife <wxmylife@gmail.com>
2017-03-31 22:46:56 +0800

    2017.3.31 22:46

--- Android_code/JNI/javacallc/src/main/java/com/wxmylife/javacallc/JNI.java
+++ Android_code/JNI/javacallc/src/main/java/com/wxmylife/javacallc/JNI.java
@@ -5,4 +5,13 @@ package com.wxmylife.javacallc;
  */
 
 public class JNI {
+
+    public native int add(int x,int y);
+
+    public native String sayHello(String s);
+
+    public native int[] increaseArrayEles(int[] array);
+
+    public native int checkPwd(String pwd);
+
 }
