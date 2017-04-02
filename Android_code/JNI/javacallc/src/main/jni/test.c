bf5f69f76a70bf9505c8920f381f8c67f02466ff
wxmylife <wxmylife@gmail.com>
2017-04-02 16:40:59 +0800

    2017.4.2 16:30

--- Android_code/JNI/javacallc/src/main/jni/test.c
+++ Android_code/JNI/javacallc/src/main/jni/test.c
@@ -27,7 +27,6 @@ char *_JString2CStr(JNIEnv *env, jstring jstr) {
 
 jint Java_com_wxmylife_javacallc_JNI_add
         (JNIEnv *env, jobject jobject, jint x, jint y) {
-
     int result = x + y;
     return result;
 }
@@ -35,13 +34,10 @@ jint Java_com_wxmylife_javacallc_JNI_add
 
 jstring Java_com_wxmylife_javacallc_JNI_sayHello
         (JNIEnv *env, jobject jobject, jstring text) {
-
     char *fromJava = _JString2CStr(env, text);
-
     char *fromC = "yes i love you,too";
     //拼接函数strcat
     strcat(fromJava, fromC);//把拼接的结果放在第一个参数中
-
     return (*env)->NewStringUTF(env, fromJava);
 }
 
