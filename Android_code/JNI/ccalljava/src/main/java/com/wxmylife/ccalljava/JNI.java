faf53a73d0ffc36b5e623e1b4c4e2e8381357509
wxmylife <wxmylife@gmail.com>
2017-04-04 23:46:28 +0800

    2017.4.4 23:46

--- Android_code/JNI/ccalljava/src/main/java/com/wxmylife/ccalljava/JNI.java
+++ Android_code/JNI/ccalljava/src/main/java/com/wxmylife/ccalljava/JNI.java
@@ -1,11 +1,15 @@
 package com.wxmylife.ccalljava;
 
+import android.util.Log;
+
+
 /**
  * Created by wxmylife on 2017/4/4.
  */
 
 public class JNI {
 
+    private static final String TAG=JNI.class.getSimpleName();
 
     {
         System.loadLibrary("ccalljava");
@@ -17,10 +21,22 @@ public class JNI {
 
     public native void callBackPrintString();
 
-    public native void callBacksayHello();
+    public native void callBackSayHello();
 
     public int add(int x,int y){
+        Log.e(TAG,"add()");
         return x+y;
     }
 
+    public void helloFromJava(){
+        Log.e(TAG,"HelloFromJava()");
+    }
+
+    public void printString(String s){
+        Log.e(TAG,"c中输入的s---->"+s);
+    }
+
+    public static void sayHello(String s){
+        Log.e(TAG,"我是java代码中的JNI."+"java中的sayHello(String s)静态方法，被c调用."+s);
+    }
 }
